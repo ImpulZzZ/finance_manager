a8342c8896e63110dd69ddea4b8fbe196503af23
ImpulZzZ <lukas.drothler@web.de>
2020-12-27 15:17:53 +0100

    initial welcome gui gui packages

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from PyQt5.QtWidgets import *
+from gui import welcome
+
+
 def main():
 
-    print("Hello World")
+    app = QApplication([])
+    MainWindow = QMainWindow()
+    ui = welcome.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    app.exec_()
 
 if __name__ == '__main__':
     main()
