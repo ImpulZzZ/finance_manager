b39a7acfa7351d9cb2f7e3ae9049aa648605072b
ImpulZzZ <lukas.drothler@web.de>
2021-01-02 18:56:21 +0100

    added logic to add new entries fixed problems with if, if, else

--- main.py
+++ main.py
@@ -66,7 +66,71 @@ def run_main_gui(loaded_entries):
     def run_new_entry_gui():
         # make new instance of entry gui
         popup.setupUi(popup_window)
+        popup.ok_button.clicked.connect(add_entry)
         popup_window.show()
+
+    ##############################################################################
+    # adds a new entry
+    def add_entry():
+        name = popup.name_field.text()
+
+        amount = int(popup.amount_field.text())
+
+        date = popup.date.text()
+        date = date.split('.')
+        date = datetime.date(int(date[2]) + 2000, int(date[1]), int(date[0]))
+
+        # only set last date if box is checked
+        if popup.checkBox_last_date.isChecked():
+            last_date = popup.last_date.text()
+        else:
+            last_date = "1.1.01"
+
+        last_date = last_date.split('.')
+        last_date = datetime.date(int(last_date[2]) + 2000, int(last_date[1]), int(last_date[0]))
+
+        monthly = False
+        yearly = False
+
+        # set monthly or yearly on chosen value
+        if popup.choose_monthly.isChecked():
+            monthly = True
+        if popup.choose_yearly.isChecked():
+            yearly = True
+
+        # create new entry object
+        new_entry = Entry(name, amount, monthly, yearly, date, last_date)
+
+        # add entry to entries
+        entries.append(new_entry)
+
+        position = ui.tableWidget.rowCount()
+
+        # add the entry details into the table
+        ui.tableWidget.setRowCount(position + 1)
+
+        if new_entry.yearly:
+            new_entry_rotation = "yearly"
+        if new_entry.monthly:
+            new_entry_rotation = "monthly"
+        else:
+            new_entry_rotation = "once"
+
+        # make strings as QTableWidgetItems
+        new_entry_name = QTableWidgetItem(new_entry.name)
+        new_entry_amount = QTableWidgetItem(str(new_entry.amount))
+        new_entry_pay_rotation = QTableWidgetItem(new_entry_rotation)
+        new_entry_pay_date = QTableWidgetItem(str(new_entry.date))
+        new_entry_last_pay_date = QTableWidgetItem(str(new_entry.last_date))
+
+        # add the entry details into the table
+        ui.tableWidget.setItem(position, 0, new_entry_name)
+        ui.tableWidget.setItem(position, 1, new_entry_amount)
+        ui.tableWidget.setItem(position, 2, new_entry_pay_rotation)
+        ui.tableWidget.setItem(position, 3, new_entry_pay_date)
+        ui.tableWidget.setItem(position, 4, new_entry_last_pay_date)
+
+        popup_window.close()
 
     ##############################################################################
 
@@ -81,6 +145,7 @@ def run_main_gui(loaded_entries):
 
     # an entry has 5 info to be shown
     ui.tableWidget.setColumnCount(ENTRY_INFORMATION_AMOUNT)
+    ui.tableWidget.setHorizontalHeaderLabels(["Name", "Amount", "Rythm", "Date", "Last Date"])
 
     # loop over the array of entries
     counter = 0
@@ -89,12 +154,12 @@ def run_main_gui(loaded_entries):
         # change table size dynamically
         ui.tableWidget.setRowCount(ui.tableWidget.rowCount() + 1)
 
+        rotation = "once"
+
         if entry.yearly:
             rotation = "yearly"
         if entry.monthly:
             rotation = "monthly"
-        if not entry.monthly and not entry.yearly:
-            rotation = "once"
 
         # make strings as QTableWidgetItems
         entry_name = QTableWidgetItem(entry.name)
@@ -142,8 +207,8 @@ def main():
     for x in range(0, 60):
         # insert a test entry
         test = "Test " + str(x)
-        entries.append(Entry(name=test, amount=512, monthly=False, yearly=True,
-                             date=datetime.date(2021, 11, 27), last_date=datetime.date(2021, 12, 27)))
+        # entries.append(Entry(name=test, amount=512, monthly=False, yearly=True,
+        #                     date=datetime.date(2021, 11, 27), last_date=datetime.date(2021, 12, 27)))
 
     run_main_gui(entries)
 
